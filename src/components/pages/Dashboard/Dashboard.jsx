6c5a8f11b103914a06cdf6ab89e595f6859707b5
meidhika <meidhikanawa@gmail.com>
2026-04-10 21:23:02 +0700

    add sidebar layout

--- src/components/pages/Dashboard/Dashboard.jsx
+++ src/components/pages/Dashboard/Dashboard.jsx
@@ -1,30 +1,19 @@
-import { Box, Button } from '@mui/material';
-import { useState } from 'react';
+import { colors, Paper, Typography } from '@mui/material';
 
-import Modal from '../../ui/Modal';
+import SidebarLayout from '../../layouts/SidebarLayout';
 
 const Dashboard = () => {
-  const [openModal, setOpenModal] = useState();
-
-  const handleClose = () => setOpenModal(false);
-  const handleOpenModal = () => setOpenModal(true);
-
   return (
-    <Box>
-      <Button type="button" variant="contained" onClick={handleOpenModal}>
-        Open Modal
-      </Button>
-      <Modal open={openModal} handleClose={handleClose} title={'Judul Modal'}>
-        <Box
-          sx={{
-            padding: 2,
-            width: 500,
-          }}
-        >
-          Isi Modal
-        </Box>
-      </Modal>
-    </Box>
+    <SidebarLayout pageTitle="Dashboard" breadcrumbs={[]}>
+      <Paper
+        sx={{
+          padding: 2,
+          background: colors.lightBlue[100],
+        }}
+      >
+        <Typography variant="h5">Menampilkan Dashboard disini</Typography>
+      </Paper>
+    </SidebarLayout>
   );
 };
 
