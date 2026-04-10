c3483d19402100d486de2e01f4bcabde3334e8a3
meidhika <meidhikanawa@gmail.com>
2026-04-10 20:18:39 +0700

    add text field

--- src/components/pages/Auth/Login/Login.jsx
+++ src/components/pages/Auth/Login/Login.jsx
@@ -2,33 +2,41 @@ import { Paper, Stack } from '@mui/material';
 import { useForm } from 'react-hook-form';
 
 import Select from '../../../ui/Forms/Select';
+import TextField from '../../../ui/Forms/TextField/TextField';
 
 const Login = () => {
   const { control } = useForm();
+
   return (
     <Stack
       spacing={2}
       alignItems={'center'}
       justifyContent={'center'}
-      sx={{ height: '100vh' }}
+      height={'100vh'}
     >
-      <Paper sx={{ width: 600, padding: 2 }}>
+      <Paper
+        sx={{
+          width: 600,
+          padding: 2,
+        }}
+      >
+        <TextField name="username" control={control} label={'Username'} />
         <Select
-          name={'kategori'}
+          name={'category'}
           control={control}
           label={'Pilih Kategori'}
           options={[
             {
-              label: 'Kategori 1',
               value: 'Kategori 1',
+              label: 'Kategori 1',
             },
             {
-              label: 'Kategori 2',
               value: 'Kategori 2',
+              label: 'Kategori 2',
             },
             {
-              label: 'Kategori 3',
               value: 'Kategori 3',
+              label: 'Kategori 3',
             },
           ]}
         />
